84de8bdb397cc37ecd5ba82507ff1c0f51667529
moaiddip@gmail.com <moaiddip@gmail.com@a6a41537-a88c-c75c-8258-1fac70e65831>
2014-11-04 19:20:06 +0000

    Kevin's alarm corrected. Check method re-done. Not completed yet.

--- trunk/smarter-house-arduino/Visual Micro/.SMARTerHouse.vsarduino.h
+++ trunk/smarter-house-arduino/Visual Micro/.SMARTerHouse.vsarduino.h
@@ -5,7 +5,7 @@
 	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: Arduino Duemilanove w/ ATmega328, Platform=avr, Package=arduino
+	Hardware: Arduino Uno, Platform=avr, Package=arduino
 */
 
 #ifndef _VSARDUINO_H_
@@ -47,7 +47,7 @@ void TestRequest(String command);
 void CheckRequest(String command);
 void MsgHandler(String command);
 void CheckAll();
-void CheckStatus(String what);
+String CheckStatus(String what);
 
 #include "C:\Program Files (x86)\Arduino\hardware\arduino\cores\arduino\arduino.h"
 #include "C:\Program Files (x86)\Arduino\hardware\arduino\variants\standard\pins_arduino.h" 
